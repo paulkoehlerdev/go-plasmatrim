5d7a0cd95a1906ba7eda242b2aae4a01e8795b85
Paul Köhler <koehler_paul@outlook.de>
2026-04-04 08:30:17 +0200

    add ssh server

--- cmd/ssh/main.go
+++ cmd/ssh/main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"io"
 	"log"
+	"log/slog"
 
 	"github.com/gliderlabs/ssh"
 )
 
 func main() {
+	addr := ":2222"
+
+	slog.Info("Starting SSH server", "addr", addr)
+
 	ssh.Handle(func(s ssh.Session) {
 		io.WriteString(s, "Hello world\n")
+		slog.Info("SSH session", "addr", s.RemoteAddr().String(), "user", s.User())
 	})
 
-	log.Fatal(ssh.ListenAndServe(":2222", nil))
+	log.Fatal(ssh.ListenAndServe(addr, nil))
 }
